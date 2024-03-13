224f8bbfeed0e8e9c1daea2974fc28b3f22ba5af
ding1997 <ding.jiawen@yahoo.com>
2024-03-13 22:09:18 +0800

    Alerts section, done by Jiawen

---
+++ src/pages/Alerts/Alerts.test.tsx
@@ -0,0 +1,30 @@
+//Reference: https://testing-library.com/docs/react-testing-library/intro/
+//Reference: https://github.com/testing-library/jest-dom
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Alerts from './Alerts';
+import '@testing-library/jest-dom/extend-expect';
+
+
+test('renders Alerts component', () => {
+  render(<Alerts />);
+  
+  const zone1Element = screen.getByText(/Zone 1/i);
+  expect(zone1Element).toBeInTheDocument();
+
+  const zone2Element = screen.getByText(/Zone 2/i);
+  expect(zone2Element).toBeInTheDocument();
+
+  const zone3Element = screen.getByText(/Zone 3/i);
+  expect(zone3Element).toBeInTheDocument();
+
+  const zone4Element = screen.getByText(/Zone 4/i);
+  expect(zone4Element).toBeInTheDocument();
+
+  const zone5Element = screen.getByText(/Zone 5/i);
+  expect(zone5Element).toBeInTheDocument();
+
+  const alertsElement = screen.getByText(/Alerts/i);
+  expect(alertsElement).toBeInTheDocument();
+});
--- src/pages/Alerts/Alerts.tsx
+++ src/pages/Alerts/Alerts.tsx
@@ -1,7 +1,24 @@
+//Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+//Reference: https://reactjs.org/docs/components-and-props.html
+
 import React from 'react';
+import styles from './Alerts.module.css';
 
 function Alerts() {
-  return <h1>This is the Status Page.</h1>;
+  return (
+    <div className={styles.App}>
+      <div className={styles.sidebar}>
+        <div className={`${styles.tab} ${styles.red}`}><span className={styles.bold}>Zone 1</span></div>
+        <div className={`${styles.tab} ${styles.yellow}`}><span className={styles.bold}>Zone 2</span></div>
+        <div className={`${styles.tab} ${styles.green}`}><span className={styles.bold}>Zone 3</span></div>
+        <div className={`${styles.tab} ${styles.red}`}><span className={styles.bold}>Zone 4</span></div>
+        <div className={`${styles.tab} ${styles.yellow}`}><span className={styles.bold}>Zone 5</span></div>
+      </div>
+      <div className={styles.content}>
+        <h1>Alerts</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Alerts;
