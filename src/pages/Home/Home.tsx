5468cdc061880ca32a61a87f6a30ed8acdbc1c6e
Gurcharan Singh <gurcharansingh@student.curtin.edu.au>
2024-03-24 17:26:40 +0800

    Added Title and Blocks to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import './Home.css';
+import InteractiveMap from '../Map/InteractiveMap'; // Import the InteractiveMap component
+
 
 function Home() {
   return (
   <div>
     <Header title="Dashboard" activeLink="Home" />
+    
+    <div className="centered-h1">
+        <h1>Welcome to AI Dashboard</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="block">
+            <h2>Alerts</h2>
+          </div>
+          <div className="block">
+            <h2>Map</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="block">
+            <h2>Status</h2>
+          </div>
+          <div className="block">
+            <h2>Block 4</h2>
+          </div>
+        </div>
+      </div>
+  
   </div>
+  
   );
 }
 
