eaecd08e9ea0dc6c5c36d409350cc30a02c4ae0f
gavenpwk <gavenphoon@hotmail.com>
2024-04-01 15:22:49 +0800

    Changes to Upload.tsx and Upload.css.

--- src/pages/Upload/Upload.tsx
+++ src/pages/Upload/Upload.tsx
@@ -5,22 +5,23 @@ import './Upload.css';
 
 function Upload() {
   const [address, setAddress] = useState({
+    country:'',
     street: '',
     city: '',
     state: '',
-    postalCode: ''
+    postcode: ''
   });
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files && event.target.files[0];
     if (file) {
-      // Check if the file type is .xlsx or .tiff
+      // Check file type is .xlsx or .tiff
       if (
         file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
         file.type === 'image/tiff'
       ) {
-        // Handle the file upload here
+        // Handle file upload here
         console.log('File Uploaded:', file);
       } else {
         alert('Please upload only .xlsx or .tiff files.');
@@ -53,23 +54,29 @@ function Upload() {
           style={{ display: 'none' }}
           ref={fileInputRef}
         />
-        {/* Address fields */}
-        <div>
-          <label>Street:</label>
-          <input type="text" name="street" value={address.street} onChange={handleAddressChange} />
+        <h3> Location: </h3>
+        {/* Address Fields (Country/Address/City/State/Postcode) */}
+        <div className="address-field">
+          <label className="address-label">Country/Region: </label>
+          <input className="address-input" type="text" name="country" value={address.country} onChange={handleAddressChange} />
         </div>
-        <div>
-          <label>City:</label>
-          <input type="text" name="city" value={address.city} onChange={handleAddressChange} />
+        <div className="address-field">
+          <label className="address-label">Street: </label>
+          <input className="address-input" type="text" name="street" value={address.street} onChange={handleAddressChange} />
         </div>
-        <div>
-          <label>State:</label>
-          <input type="text" name="state" value={address.state} onChange={handleAddressChange} />
+        <div className="address-field">
+          <label className="address-label">City: </label>
+          <input className="address-input" type="text" name="city" value={address.city} onChange={handleAddressChange} />
         </div>
-        <div>
-          <label>Postal Code:</label>
-          <input type="text" name="postalCode" value={address.postalCode} onChange={handleAddressChange} />
+        <div className="address-field">
+          <label className="address-label">State/Territory: </label>
+          <input className="address-input" type="text" name="state" value={address.state} onChange={handleAddressChange} />
         </div>
+        <div className="address-field">
+          <label className="address-label">Postcode: </label>
+          <input className="address-input" type="text" name="postcode" value={address.postcode} onChange={handleAddressChange} />
+        </div>
+        
         {/* Upload button */}
         <ContainedButton onClick={handleClick} label="Upload File" />
       </div>
