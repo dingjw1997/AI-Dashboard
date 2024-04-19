2671242b650e7ec999532f9297aaf8b65fd477d4
FranDiGio <francodvf06@gmail.com>
2024-04-19 15:51:02 +0800

    Delete ContainedButton and its references

--- src/pages/Upload/Upload.tsx
+++ src/pages/Upload/Upload.tsx
@@ -1,10 +1,7 @@
 /* eslint-disable no-lone-blocks */
 import React, { useRef, useState } from 'react';
 import { TextField, Stack, Typography, Button } from '@mui/material';
-
-
 import Header from '../../components/Header/Header';
-import ContainedButton from '../../components/ContainedButton/ContainedButton';
 
 function Upload() {
 
